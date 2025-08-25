9404b427d62e3d488d6bfcf862db4caeb797f82b
andresequez <andresequez@gmail.com>
2025-08-25 17:34:45 -0600

    fix: default currency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -156,7 +156,7 @@ export default function Home() {
       <DetailedFeatures />
       <ValueStripe />
       <WhyMidiMed />
-      <PricingSection defaultCurrency="GTQ" />
+      <PricingSection />
       <FinalCTA />
       <FAQ />
       <Testimonials />
